e4b2614e400aa04a6047dbe0f0a2f1b20b6763d2
Felipe <felipemarques.computacao@gmail.com>
2024-09-21 09:32:10 -0300

    feat: new validation pipe conditions

--- backend/codenews-backend/src/main.ts
+++ backend/codenews-backend/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+   app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Remove propriedades não validadas
+    forbidNonWhitelisted: true, // Proíbe propriedades não permitidas
+    transform: true, // Transforma os dados recebidos em classes correspondentes
+  }));
 
   await app.listen(3000);
 }
