36b58a8c700123069db1eed9c106798b4f655063
Felipe <felipemarques.computacao@gmail.com>
2024-09-22 09:56:09 -0300

    fix: CORS policy

--- backend/codenews-backend/src/main.ts
+++ backend/codenews-backend/src/main.ts
@@ -5,12 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove propriedades não validadas
-    forbidNonWhitelisted: true, // Proíbe propriedades não permitidas
-    transform: true, // Transforma os dados recebidos em classes correspondentes
-  }));
-
-  await app.listen(3001);
+  app.enableCors({
+  origin: 'http://localhost:3000',  // Origem permitida
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+  });
+  app.enableCors();
+  await app.listen(8000);
 }
 bootstrap();
