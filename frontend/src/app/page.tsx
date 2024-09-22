3bed83651b6c0be13cff40330862454cd135603c
Felipe <felipemarques.computacao@gmail.com>
2024-09-22 09:05:57 -0300

    feat: using next.js in frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <div>
+      <h1 className="flex justify-center">Welcome, Young Padawan!</h1>
+   </div> 
+  );
+}
---
+++ frontend/src/app/pages/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1 className="text-2xl">Hello, Dashboard page!</h1>
+  ); 
+}
