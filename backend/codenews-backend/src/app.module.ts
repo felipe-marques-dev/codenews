4fcc24800bd52c7a2687d149b4b8249047b21193
Felipe <felipemarques.computacao@gmail.com>
2024-09-23 23:03:13 -0300

    feat: static path for images

--- backend/codenews-backend/src/app.module.ts
+++ backend/codenews-backend/src/app.module.ts
@@ -5,12 +5,19 @@ import { UserService } from './modules/user/user.service';
 import { PostService } from './modules/post/post.service';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
-
-
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [UserModule, PostModule],
+  imports: [
+    UserModule,
+    PostModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), // Caminho da pasta uploads
+      serveRoot: '/uploads/', // URL base para acessar os arquivos
+    }),
+  ],
   controllers: [AppController],
-  providers: [UserService, PostService,PrismaService],
+  providers: [UserService, PostService, PrismaService],
 })
-export class AppModule {}
+export class AppModule {
+}
