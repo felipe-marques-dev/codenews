1de5bf2d02c57b323a5bdc7121ce3678b2df5610
Felipe <felipemarques.computacao@gmail.com>
2024-09-24 00:28:27 -0300

    style: images size and page colors

--- frontend/src/app/pages/posts/page.tsx
+++ frontend/src/app/pages/posts/page.tsx
@@ -8,6 +8,7 @@ import { client } from "@/services/client";
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import "../../globals.css";
 
 interface PostProps{
     posts: Post[];
@@ -41,31 +42,39 @@ export default function PostsPage (){
           <h1 className="text-3xl font-bold mb-8 m-4">Latest Blog Posts</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {posts.map((post) => (
-              <Card key={post.id} className="flex flex-col m-3">
-                <div className="relative w-full h-48 p-2">
+              <Card key={post.id} className="flex flex-col m-3"
+                style={{
+                    cursor: 'pointer',
+                    borderRadius: '8px',
+                    backgroundColor: "#202020"
+                }}
+              >
+                <div className="relative w-full h-62 p-4 pb-0 bg-cbackground">
                 <img
                 src={`http://localhost:8000/${post.imageUrl}`}
                 style={{
-                    maxHeight: '15vh',
-                    maxWidth: 'auto',
-                    borderRadius: '6px'
+                    maxHeight: '120%',
+                    height: '100%',
+                    maxWidth: '100vw',
+                    width: '100%',
+                    borderRadius: '8px',
                 }}
                 alt={`Cover image for ${post.title}`}
                 className="object-cover"
                 />
             </div>
                 <CardHeader>
-                  <h2 className="text-xl font-semibold">{post.title}</h2>
+                  <h2 className="text-2xl font-semibold">{post.title}</h2>
                 </CardHeader>
                 <CardContent className="flex-grow">
-                  <p className="text-muted-foreground">{post.content}</p>
+                  <p className="text-muted-foreground text-lg">{post.content}</p>
                 </CardContent>
                 <CardFooter className="flex items-center justify-between">
                   <div className="flex items-center space-x-2">
                     <Avatar>
                       <AvatarFallback></AvatarFallback>
                     </Avatar>
-                    <span className="text-sm font-medium">{post.author.name}</span>
+                    <span className="text-md font-medium">{post.author.name}</span>
                   </div>
                   { /* <span className="text-sm text-muted-foreground">{post.date}</span> */}
                 </CardFooter>
