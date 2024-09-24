81e4775986f6053035ab697e95e64a292b692db5
Felipe <felipemarques.computacao@gmail.com>
2024-09-24 20:09:12 -0300

    feat: added posts component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+import PostsPage from "./pages/posts/page";
+
+export default function Page() {
   return (
-   <div>
-      <h1 className="flex justify-center">Welcome, Young Padawan!</h1>
-   </div> 
-  );
+    <div className="container mx-auto py-8 m-3 lg:w-1/2">
+          <h1 className="text-5xl font-bold mb-8 m-4">Latest Blog Posts</h1>
+          <PostsPage />
+    </div>
+  ); 
 }
